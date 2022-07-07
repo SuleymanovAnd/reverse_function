8301fa872d5703b13267f25c5780c38759ca38e2
Suleymanov Andrey <sasus37@gmail.com>
2022-07-07 20:07:30 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+void reverse (int* ar){
+    for (int i = 0; i<6;i++){
+        int temp = *(ar + (9-i));
+        *(ar + (9-i)) = *(ar+i);
+        *(ar+i) = temp;
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int array [10] = {10,9,8,7,6,5,4,3,2,1};
+    int* pAr = array;
+     reverse (pAr);
+    for (int i = 0; i <10;i++){
+        std :: cout << *(pAr +i)<<" ";
+    }
 }
